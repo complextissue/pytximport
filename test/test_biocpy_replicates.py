735521003cda6d45ee9be386c64df976d9c32b01
Jayaram Kancherla <jayaram.kancherla@gmail.com>
2024-09-30 11:21:57 -0700

    Add a similar test to biocpy for replicates

--- test/test_biocpy_replicates.py
+++ test/test_biocpy_replicates.py
@@ -3,15 +3,15 @@
 from pathlib import Path
 from typing import List
 
-import anndata as ad
+from summarizedexperiment import SummarizedExperiment
 
 from pytximport import tximport
 
 
-def test_anndata_replicates(
+def test_biocpy_replicates(
     fabry_disease_files: List[Path],
 ) -> None:
-    """Test importing quantification files with inferential replicates with AnnData output.
+    """Test importing quantification files with inferential replicates with SummarizedExperiment output.
 
     Args:
         fabry_disease_files (List[Path]): The paths to the quantification files.
@@ -24,16 +24,16 @@ def test_anndata_replicates(
         fabry_directory / "transcript_gene_mapping_human.tsv",
         ignore_transcript_version=True,
         ignore_after_bar=True,
-        output_type="anndata",
+        output_type="summarizedexperiment",
         inferential_replicates=True,
         inferential_replicate_variance=True,
         inferential_replicate_transformer=None,  # Do not recalculate counts but include the inferential replicates
     )
 
-    assert isinstance(result, ad.AnnData), "The result is not an AnnData object."
+    assert isinstance(result, SummarizedExperiment), "The result is not an SummarizedExperiment object."
 
-    # Check that variance is in the obsm
-    assert "variance" in result.obsm.keys()
+    # Check that variance is in the metadata
+    assert "variance" in result.get_metadata()
 
-    # Check that inferential replicates is in the uns
-    assert "inferential_replicates" in result.uns.keys()
+    # Check that inferential replicates is in the metadata
+    assert "inferential_replicates" in result.get_metadata()
